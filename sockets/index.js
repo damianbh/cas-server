9d687699cf31d5de4639644b7ab6c1d502866d2d
Damián Bergantiños <damianbh@gmail.com>
2015-05-16 00:19:46 -0400

    Refactor sockets implementation to a more simple approach

--- sockets/index.js
+++ sockets/index.js
@@ -2,8 +2,8 @@
  * Created by damian on 4/26/2015.
  */
 var
-    _ = require('underscore'),
-    hashClients = {};
+    _ = require('underscore');
+//    ,hashClients = {};
 
 module.exports = {
     init: function (server) {
@@ -12,47 +12,50 @@ module.exports = {
         io.on('connection', function (socket) {
             //socket.emit('news', {hello: 'world'});
             socket.on('new_connection', function (data) {
-                var
-                    arr = hashClients[data.session] = _.isArray(hashClients[data.session]) ? hashClients[data.session] : [];
-
-                if (arr.indexOf(socket) === -1) {
-                    arr.push(socket);
-                }
+                socket.join(data.session);
+                //                var
+                //                    arr = hashClients[data.session] = _.isArray(hashClients[data.session]) ? hashClients[data.session] : [];
+                //
+                //                if (arr.indexOf(socket) === -1) {
+                //                    arr.push(socket);
+                //                }
             });
             socket.on('login', function (data) {
-                module.exports.emit2Session(data.session, 'login', socket);
+                socket.broadcast.to(data.session).emit('login');
+//                module.exports.emit2Session(data.session, 'login', socket);
             });
             socket.on('logout', function (data) {
-                module.exports.emit2Session(data.session, 'logout', socket);
+                socket.broadcast.to(data.session).emit('logout');
+//                module.exports.emit2Session(data.session, 'logout', socket);
             });
-            socket.on('disconnect', function () {
-                _.each(_.keys(hashClients), function (session) {
-                    if (_.isArray(hashClients[session])) {
-                        hashClients[session] = _.without(hashClients[session], socket);
-                        if (hashClients[session].length === 0) {
-                            delete hashClients[session];
-                        }
-                    } else {
-                        delete hashClients[session];
-                    }
-
-                });
-            });
+            //            socket.on('disconnect', function () {
+            //                _.each(_.keys(hashClients), function (session) {
+            //                    if (_.isArray(hashClients[session])) {
+            //                        hashClients[session] = _.without(hashClients[session], socket);
+            //                        if (hashClients[session].length === 0) {
+            //                            delete hashClients[session];
+            //                        }
+            //                    } else {
+            //                        delete hashClients[session];
+            //                    }
+            //
+            //                });
+            //            });
 
         });
 
     },
-    emit2Session: function (session, event, socket, data) {
-        var
-            arr = hashClients[session] || [];
-
-        if (socket) {
-            arr = _.without(arr, socket);
-        }
-        _.each(arr, function (socket) {
-            socket.emit(event, data);
-        });
-    },
+    //    emit2Session: function (session, event, socket, data) {
+    //        var
+    //            arr = hashClients[session] || [];
+    //
+    //        if (socket) {
+    //            arr = _.without(arr, socket);
+    //        }
+    //        _.each(arr, function (socket) {
+    //            socket.emit(event, data);
+    //        });
+    //    },
     getHashSockets: function () {
         var
             result = {};
